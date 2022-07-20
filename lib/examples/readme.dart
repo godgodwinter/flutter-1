3bd2f55ce330a16a3892b2106723d4143ed3cf50
godgodwinter <kakadz.pro@gmail.com>
2022-07-20 16:09:33 +0700

    Initial commit

---
+++ lib/examples/readme.dart
@@ -0,0 +1,11 @@
+//examples
+//!SIMPLE-ROUTES
+// 1-2. Simple routes
+// 3. Named routes
+// 4. Named routes with arguments 
+// 5. returning data https://docs.flutter.dev/cookbook/navigation/returning-data
+//
+
+// ! DRAWER
+//  main
+//  navbar 
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:myapp2/splashscreen_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App Fluterku',
+      home: SplashScreenPage(),
+    );
+  }
+}
